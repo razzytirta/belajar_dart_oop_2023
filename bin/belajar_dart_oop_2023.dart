f4f62be861fdd371d2558a090921a7c12ae73d7e
razzytirta <azzi.fachrurazzi@gmail.com>
2023-11-24 05:19:45 +0800

    Belajar Dart OOP 2023

---
+++ bin/belajar_dart_oop_2023.dart
@@ -0,0 +1,5 @@
+import 'package:belajar_dart_oop_2023/belajar_dart_oop_2023.dart' as belajar_dart_oop_2023;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_dart_oop_2023.calculate()}!');
+}
---
+++ test/belajar_dart_oop_2023_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_dart_oop_2023/belajar_dart_oop_2023.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
